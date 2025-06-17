aebae1de7b010f5ccd1fb5d44e06d5d404a24a79
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-06-17 18:26:07 +0800

    Create class for rappler webscraper

--- webcrawling/rappler_scraper.py
+++ webcrawling/rappler_scraper.py
@@ -3,45 +3,66 @@ from bs4 import BeautifulSoup
 import csv
 
 from search_articles import Search_articles
-search_news = Search_articles()
-news_urls = (search_news.search_news("LIST: Schools, organizations pushing Senate to proceed with VP Sara impeachment"))
-for i in news_urls:
-    print(i)
 
 num_links = 0
 
-headline_tag1 = "h1"
-headline_tag2 = "h3"
-content_tag = "p"
 
-#im passing soup on each function parameter for reusability later, as the heading = soup same for content, if we want to repeat the same function without
-#rebuilding the whole thing passing soup would be beneficial
+# im passing soup on each function parameter for reusability later, as the heading = soup same for content, if we want to repeat the same function without
+# rebuilding the whole thing passing soup would be beneficial
 
 
-for i in news_urls:
-    response = requests.get(i)
-    soup = BeautifulSoup(response.text, "html.parser")
-    #HEADLINE
-    def headeline(soup, tags = [headline_tag1, headline_tag2]):
+class RapplerScraper:
+    HEADLINE_TAG1 = "h1"
+    HEADLINE_TAG2 = "h3"
+    CONTENT_TAG = "p"
+
+    def scrape_urls(self, news_urls):
+        for news_url in news_urls:
+            response = requests.get(news_url)
+            soup = BeautifulSoup(response.text, "html.parser")
+            soup.select()
+            soup.find_all()
+            print(news_url)
+            print(self.get_headline(soup))
+            print(self.get_content(soup))
+            print()
+
+    # Returns a list of headlines
+    def get_headline(self, soup, tags=[]):
+        # Set optional parameter
+        if tags == []:
+            tags = [self.HEADLINE_TAG1, self.HEADLINE_TAG2]
+
         for tag in tags:
-            heading = soup.find_all(tag)  
-            n=len(heading)
+            headings = soup.find_all(tag)
+
+            n = len(headings)
             if n == 0:
-                print(f"No headline of {tag} found")
+                # No headlines found
+                return []
             else:
-                for i in range(n):
-                    print(str.strip(heading[i].text))
+                headlines = []
+                for heading in headings:
+                    headlines.append(str.strip(heading.text))
+                return headlines
 
-    #CONTENT
-    def content(soup):
-        main_content = soup.find_all(content_tag)
-        n=len(main_content)
+    # Return a string of the content.
+    def get_content(self, soup):
+        main_content = soup.find_all(self.CONTENT_TAG)
+        soup.select
+        n = len(main_content)
         if n <= 1:
-            print(f"No content of {content_tag} found")
+            # No content found
+            return ""
         else:
-            for i in range(n):
-                print(F" {str.strip(main_content[i].text)}")
+            return " ".join(str.strip(content.text) for content in main_content)
+            # for i in range(n):
+            #     print(f" {str.strip(main_content[i].text)}")
 
-    headeline(soup)
-    content(soup)
- 
+
+# search_news = Search_articles()
+# news_urls = search_news.search_news(
+#     "LIST: Schools, organizations pushing Senate to proceed with VP Sara impeachment"
+# )
+# rap_scrap = RapplerScraper()
+# rap_scrap.scrape_urls(news_urls)
