7f3dd348c23fbf5a52493ec57852f2cc80bec5d0
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-06-17 20:57:13 +0800

    Scrape urls now return a dictionary of headline and content

--- webcrawling/rappler_scraper.py
+++ webcrawling/rappler_scraper.py
@@ -9,23 +9,21 @@ num_links = 0
 
 # im passing soup on each function parameter for reusability later, as the heading = soup same for content, if we want to repeat the same function without
 # rebuilding the whole thing passing soup would be beneficial
-
-
 class RapplerScraper:
     HEADLINE_TAG1 = "h1"
     HEADLINE_TAG2 = "h3"
     CONTENT_TAG = "p"
 
     def scrape_urls(self, news_urls):
+        scraped_data = {}
         for news_url in news_urls:
             response = requests.get(news_url)
             soup = BeautifulSoup(response.text, "html.parser")
-            soup.select()
-            soup.find_all()
-            print(news_url)
-            print(self.get_headline(soup))
-            print(self.get_content(soup))
-            print()
+            scraped_data[news_url] = {
+                "headline": self.get_headline(soup)[0],
+                "content": self.get_content(soup),
+            }
+        return scraped_data
 
     # Returns a list of headlines
     def get_headline(self, soup, tags=[]):
@@ -33,31 +31,26 @@ class RapplerScraper:
         if tags == []:
             tags = [self.HEADLINE_TAG1, self.HEADLINE_TAG2]
 
+        headlines = []
         for tag in tags:
             headings = soup.find_all(tag)
-
-            n = len(headings)
-            if n == 0:
-                # No headlines found
-                return []
-            else:
-                headlines = []
+            if len(headings) != 0:
                 for heading in headings:
                     headlines.append(str.strip(heading.text))
-                return headlines
+        return headlines
 
     # Return a string of the content.
     def get_content(self, soup):
-        main_content = soup.find_all(self.CONTENT_TAG)
-        soup.select
-        n = len(main_content)
-        if n <= 1:
+        content_containers = soup.find_all("div", class_="entry-content")
+        main_content = []
+        for container in content_containers:
+            found_content = container.find_all(self.CONTENT_TAG)
+            main_content += found_content
+        if len(main_content) <= 1:
             # No content found
             return ""
         else:
             return " ".join(str.strip(content.text) for content in main_content)
-            # for i in range(n):
-            #     print(f" {str.strip(main_content[i].text)}")
 
 
 # search_news = Search_articles()
@@ -65,4 +58,4 @@ class RapplerScraper:
 #     "LIST: Schools, organizations pushing Senate to proceed with VP Sara impeachment"
 # )
 # rap_scrap = RapplerScraper()
-# rap_scrap.scrape_urls(news_urls)
+# print(rap_scrap.scrape_urls(news_urls))
