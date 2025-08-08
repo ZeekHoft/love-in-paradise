e236a1b1aeeac58a93d1d18c047e84b425e7c6eb
Nero Hoya <dltposiedon@gmail.com>
2025-08-08 23:01:55 +0800

    added the feature of showing data in next js

--- love_in_paradise/server/server.py
+++ love_in_paradise/server/server.py
@@ -4,7 +4,10 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from main import love_in_paradise
 
-print(love_in_paradise)
+
+def favorite_food():
+    return("cheesecake")
+
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
@@ -12,15 +15,12 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 @app.route("/api/home", methods=['GET'])
 def return_home():
     return jsonify({
-        'message': love_in_paradise,
-        'favorite_dog' : "Tiger"
+        'message': love_in_paradise(),
+        'favorite_dog' : favorite_food()
     })
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)#remove when release
-
-
-
 
 
 
