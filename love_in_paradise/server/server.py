a000b371a27ea416eb44dc07699111935824192a
Nero Hoya <dltposiedon@gmail.com>
2025-08-12 00:23:57 +0800

    finished GET and POST

--- love_in_paradise/server/server.py
+++ love_in_paradise/server/server.py
@@ -13,16 +13,32 @@ CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route("/api/home", methods=['GET', 'POST'])
-def return_home():
+
+#POST
+
+def handle_post_request():
+  data = request.get_json()
+  if data:
+    name = data.get('name')
+    # return jsonify({"message": f"Hello, {name}!",
+    #                 }), 200
     return jsonify({
-        # 'message': love_in_paradise(),
-        'favorite_dog' : favorite_food()
+       'message': f"ALL NEWS: \n {love_in_paradise(name)}"
     })
+
+  else:
+     return jsonify({"error": "Invalid request"}), 400
+
+
+#GET
+# def return_home():
+#     return jsonify({
+#         # 'message': love_in_paradise(),
+#         'favorite_dog' : favorite_food()
+#     })
     
 
-
-
-
+    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)#remove when release
 
