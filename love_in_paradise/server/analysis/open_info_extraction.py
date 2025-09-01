6d00e78d6871b615f00e1090d798bc54dfa8e226
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-09-01 13:12:17 +0800

    Combining of algos to main file

--- love_in_paradise/server/analysis/open_info_extraction.py
+++ love_in_paradise/server/analysis/open_info_extraction.py
@@ -15,18 +15,19 @@ class OpenInformationExtraction:
     Uses Stanza/Stanford OpenIE for extracting relations from text sentences.
     """
 
-    triples = []
-
     def generate_triples(self, text):
         # Returns a triple for each sentence
+        triples = []
         with CoreNLPClient(
             annotators=["openie", "coref"],
             start_server=StartServer.TRY_START,
             properties=PROPERTIES,
+            be_quiet=True,
         ) as client:
             annotation = client.annotate(text)
             for sentence in annotation.sentence:
                 for data in sentence.openieTriple:
                     triple = (data.subject, data.relation, data.object)
-                    print("|-", triple)
-                    self.triples.append(triple)
+                    # print("|-", triple)
+                    triples.append(triple)
+        return triples
