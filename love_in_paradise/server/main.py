f0428e77d7a4f7cc8720b9323272d0eaef95125b
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-09-28 16:02:32 +0800

    Erase duplicate section

--- love_in_paradise/server/main.py
+++ love_in_paradise/server/main.py
@@ -176,71 +176,8 @@ def love_in_paradise(claim):
                 evidence_count[label] += 1
             print("Evidences found:")
             print(evidence_count)
-
-
-
-
-
     except Exception as e:
         return (f"Error in algo here: {e}")
-
-
-
-    triples = {}
-    """
-    triples = {
-        url: [(triple), (triple)],
-    }
-    """
-    only_triples = []
-    info_ext = OpenInformationExtraction()
-    for url, sentences in relevant_sentences.items():
-        url_triples = []
-        for sent in sentences:
-            gen_triples = info_ext.generate_triples(sent)
-            if gen_triples:
-                url_triples.extend(gen_triples)
-        if url_triples != []:
-            triples[url] = url_triples
-            only_triples.extend(url_triples)
-    claim_triple = info_ext.generate_triples(claim_input)
-
-    # CLAIM-EVIDENCE ALIGNMENT & ENTAILMENT SCORING
-    # ===============================================================
-    # Given a list of the most relevant sentences from articles, evaluate them against the claim
-    # -> evidences = {"agree", "disagree", "neutral"}
-    evidence_alignment = EvidenceAlignment()
-    relevant_evidence = []
-    # get related triples
-    subjects = []
-    for ct in claim_triple:
-        subjects.append(ct[0])
-        subjects.append(ct[2])
-    print("subjects: ", subjects)
-    print("Relevant evidences:")
-    sources = []
-    for source, url_triple in triples.items():
-        for tri in url_triple:
-            if list(set(subjects) & set(tri)):
-                print(tri)
-                relevant_evidence.append(tri)
-                if source not in sources:
-                    sources.append(source)
-    # print(relevant_evidence)
-    generate_graph(relevant_evidence)
-    alignments = evidence_alignment.calculate_entailment(
-        claim_input, [" ".join(e) for e in relevant_evidence]
-    )
-    evidence_count = {
-        "neutral": 0,
-        "entailment": 0,
-        "contradiction": 0,
-    }
-    for label, score in alignments:
-        evidence_count[label] += 1
-    print("Number of evidences found:")
-    print(evidence_count)
-
 
     # AGGREGATION
     # ===============================================================
