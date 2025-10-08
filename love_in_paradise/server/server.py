03eb277cb01095544f1378c7703abab4d777ff02
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-10-08 15:25:23 +0800

    Change to adjust to dictionary return from main

--- love_in_paradise/server/server.py
+++ love_in_paradise/server/server.py
@@ -1,41 +1,29 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from main import love_in_paradise
 from flask import request
 
+from main import love_in_paradise
+
+
 def favorite_food():
-    return("cheesecake")
+    return "cheesecake"
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-@app.route("/api/home", methods=['GET', 'POST'])
+@app.route("/api/home", methods=["GET", "POST"])
 
-#POST
-
+# POST
 def handle_post_request():
-  data = request.get_json()
-  if data:
-    
-    name = data.get('name')
-    # print(f"ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss{name}")
-    return jsonify({
-       'message': f"Conclusion: \n {love_in_paradise(name)}"
-      #  'message': f"ALL NEWS: \n {(name)}"
-        
-    })
-    
+    data = request.get_json()
+    if data:
+        name = data.get("name")
+        return jsonify(love_in_paradise(name))
+    else:
+        return jsonify({"error": "Invalid request"}), 400
 
-  else:
-     return jsonify({"error": "Invalid request"}), 400
 
-    
-
-    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
-
-
-
-
