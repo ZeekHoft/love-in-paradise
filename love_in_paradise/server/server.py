8c7160029530165182b21e378d8568006831f9b1
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-10-10 01:03:39 +0800

    Edit response to return a stream with a ndjson return type

--- love_in_paradise/server/server.py
+++ love_in_paradise/server/server.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask, Response, stream_with_context
 from flask_cors import CORS
 from flask import request
+import json
 
 from main import love_in_paradise
 
@@ -13,16 +14,22 @@ app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-@app.route("/api/home", methods=["GET", "POST"])
-
 # POST
+@app.route("/api/home", methods=["GET", "POST"])
 def handle_post_request():
     data = request.get_json()
     if data:
         name = data.get("name")
-        return jsonify(love_in_paradise(name))
+        return Response(
+            stream_with_context(
+                # Convert to string ending with newline
+                json.dumps(result) + "\n"
+                for result in love_in_paradise(name)
+            ),
+            mimetype="application/x-ndjson",
+        )
     else:
-        return jsonify({"error": "Invalid request"}), 400
+        return "Invalid request", 400
 
 
 if __name__ == "__main__":
