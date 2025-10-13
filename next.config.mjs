90435216546a9bd98b277030213df9e6ea9a2b61
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-10-13 16:30:07 +0800

    Test fix for rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -12,11 +12,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8080/api/:path*"
-            : "/api/",
+        source: "/api/home",
+        destination: "/api/app.py",
       },
     ];
   },
