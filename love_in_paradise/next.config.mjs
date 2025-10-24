9be6263928c17ff2e3d08508458d4e208d77e5cf
h1tmd <105845103+h1tmd@users.noreply.github.com>
2025-10-24 23:10:16 +0800

    Change output to export

--- love_in_paradise/next.config.mjs
+++ love_in_paradise/next.config.mjs
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home",
-        permanent: true,
-      },
-    ];
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
 
